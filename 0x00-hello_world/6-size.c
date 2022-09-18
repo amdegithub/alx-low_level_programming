c4840be3e4e885c76f0008f0055c252abd3dbc06
343575cb6ec6 <amde@example.com>
2022-09-18 05:37:44 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,9 +15,9 @@ int main(void)
 	long long lli;
 	float f;
 
-	printf("size of a char: %1d byte (s)\n", sizeof(c));
-	printf("size of a int: %1d byte (s)\n", sizeof(i)); 
-       	printf("size of a long: %1d byte (s)\n", sizeof(li));
-	printf("size of a long long: %1d byte (s)\n", sizeof(lli));
-	printf("size of a float: %1d byte (s)\n", sizeof(f)); 
+	printf("size of a char: %1d byte(s)\n", sizeof(c));
+	printf("size of a int: %1d byte(s)\n", sizeof(i)); 
+       	printf("size of a long: %1d byte(s)\n", sizeof(li));
+	printf("size of a long long: %1d byte(s)\n", sizeof(lli));
+	printf("size of a float: %1d byte(s)\n", sizeof(f)); 
 }
