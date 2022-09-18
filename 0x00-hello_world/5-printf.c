4643bef97301cffab06bc4d2e6c91b9c7cef9611
343575cb6ec6 <amde@example.com>
2022-09-18 03:52:09 -0700

    print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@ include <stdio.h>
 *main - print the string in the put function  
 *
 *Description: using the main function 
-*this program prints "with proper grammar, but the outcome is a piece of art
+*this program prints "programming is like building a multilingual puzzle
 *Return: 0  
 */
 int main(void)
