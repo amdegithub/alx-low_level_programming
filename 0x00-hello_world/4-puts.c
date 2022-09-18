9dd12b68617eb9ffa2add68e1a7d45b5145b4696
343575cb6ec6 <amde@example.com>
2022-09-18 02:42:31 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
-#!/bin/bash
-gcc -c $CFILE
-#include <studio.h>
+#include <stdio.h>
 
 /**
-*main - print the string in the put function
-*this program prints "Programming is like building a multilingual puzzle
-*return: 0
+*main - print the string in put the put function
+*
+*Description: using the main function
+*this program prints "programming is like building a multilingual puzzle
+*Return: 0
 */
 int main(void)
 {
-       puts("\"programming is like building a multilingual puzzel");
-       return (O);
-}
+      puts("\"programming is like building a multilingual puzzle");
+      return: (0);
