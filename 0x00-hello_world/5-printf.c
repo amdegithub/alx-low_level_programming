aca96147322c999196f2c86b9bcbd0a67a008c1b
343575cb6ec6 <amde@example.com>
2022-09-18 03:49:32 -0700

    print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
+include <stdio.h>
 
 /**
 *main - print the string in the put function  
 *
 *Description: using the main function 
-*this program prints "programming is like building a multilingual puzzle 
+*this program prints "with proper grammar, but the outcome is a piece of art
 *Return: 0  
 */
 int main(void)
 {
-        printf("with proper grammer, but the outcame is a pice of art,\n");
+        printf("with proper grammar, but the outcome is a piece of art,\n");
         return(0);
 }
