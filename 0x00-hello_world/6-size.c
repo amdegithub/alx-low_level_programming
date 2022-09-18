9904d2c8f345c11e7b7bd1d1c67420ec5c05bf4d
343575cb6ec6 <amde@example.com>
2022-09-18 05:51:44 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,8 +17,8 @@ int main(void)
 
 	printf("size of a char: %1d byte(s)\n", sizeof(c));
 	printf("size of an int: %1d byte(s)\n", sizeof(i)); 
-       	printf("size of a long: %1d byte(s)\n", sizeof(li));
-	printf("size of a long long: %1d byte(s)\n", sizeof(lli));
+       	printf("size of a long int: %1d byte(s)\n", sizeof(li));
+	printf("size of a long long int: %1d byte(s)\n", sizeof(lli));
 	printf("size of a float: %1d byte(s)\n", sizeof(f));
 	return (0);
 }
