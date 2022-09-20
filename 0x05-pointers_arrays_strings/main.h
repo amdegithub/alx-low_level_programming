81cf04c4594e30a188a0eb22b4166c7e7ea7ab94
343575cb6ec6 <amde@example.com>
2022-09-20 12:06:14 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN.H
-#define MAIN.H 
+#ifndef MAIN_H
+#define MAIN_H 
 
 /**
 *void prototypes()
@@ -8,13 +8,13 @@
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int_strlen(char *s);
-void_puts(char *str);
+int _strlen(char *s);
+void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int_atoi(char *str);
+int _atoi(char *str);
 
