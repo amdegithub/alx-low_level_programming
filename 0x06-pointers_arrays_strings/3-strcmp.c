c6d23bb0cf942637c4132e187b6733ec79f10090
343575cb6ec6 <amde@example.com>
2022-09-22 12:19:14 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,9 +4,8 @@
  *  _strcmp - Function which compare two strings and
  *  @s1: first string
  *  @s2:second string
- *  Return:
- *  		returns zero if s1 == s2
- *  		returns negative number if s1 < s2
+ *		returns zero if s1 == s2
+ *		returns negative number if s1 < s2
  *		returns positive number if s1 > s2
  */
 
